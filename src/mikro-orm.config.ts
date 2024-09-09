101b263d8a2c96cbb03f42f307ec589a24525fb4
Bram Gussekloo <bramgussekloo@skoon.world>
2024-09-09 22:00:17 +0200

    log directory

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,6 +1,11 @@
 import { Options } from "@mikro-orm/core";
 import { Migrator } from "@mikro-orm/migrations";
 import { SqliteDriver } from "@mikro-orm/sqlite";
+import fs from "fs";
+
+fs.readdirSync(__dirname).forEach((file) => {
+    console.log(file);
+});
 
 const Config: Options = {
     entities: ["./entities"],
